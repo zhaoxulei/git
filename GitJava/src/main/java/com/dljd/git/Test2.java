7ca1e5c92bbc3699d8763d4a07ef991e61c6aedd
zhangsan <zhangsan@163.com>
2021-03-11 22:09:19 +0800

    项目2

--- GitJava/src/main/java/com/dljd/git/Test2.java
+++ GitJava/src/main/java/com/dljd/git/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("娃哈哈");
         System.out.println("项目1");
+        System.out.println("项目2");
     }
 }
